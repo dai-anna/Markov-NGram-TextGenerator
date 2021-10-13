4ebebf32cf8e351e9c69b2920d686a449c81a577
ad522 <ad522@duke.edu>
2021-10-13 01:32:15 -0400

    update for comments

--- markov_text_generator.py
+++ markov_text_generator.py
@@ -12,10 +12,13 @@ def next_word(sentence:list[str], ngram:int, corpus:list[str], deterministic=Fal
                 matched[new_match] += 1
             else:
                 matched[new_match] = 1
+    # To implement Stupid Backoff
     if len(matched) == 0:
         return next_word(sentence, ngram-1, corpus, deterministic)
+    # For deterministic = True, get the matched value with highest count and pick the first value
     if deterministic == True:
         the_chosen_one = max(matched, key=matched.get)
+    # For deterministic = False, get a random value weighted by frequency of occurrence.
     else:
         the_chosen_one = rm.choices(
             list(matched.keys()), weights=list(matched.values()))[0]
@@ -25,6 +28,7 @@ def next_word(sentence:list[str], ngram:int, corpus:list[str], deterministic=Fal
 def finish_sentence(sentence:list[str], n:int, corpus:list[str], deterministic=False):
     """completes the sentence by leveraging the next_word function"""
     final_sentence = sentence
+    # Arbitrarily cuts off sentence at 15 words if it does not hit ".", "?", "!"
     while final_sentence[-1] not in {".", "?", "!"} and len(final_sentence) < 15:
         final_sentence.append(next_word(sentence, n, corpus, deterministic))
     return final_sentence
